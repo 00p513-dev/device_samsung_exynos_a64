dbc9c4e217cce0305db32842514c2447a2defd2f
Amy <amy@amyrom.ml>
2021-08-04 19:24:10 +0000

    Enable some arch flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,1 +1,7 @@
 DEVICE_PATH := device/samsung/exynos_a64
+
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
